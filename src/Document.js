323f94556034198ebc893443f5382441dc7dd777
Samuel Elgozi <samu.elgozi@gmail.com>
2020-03-23 13:26:24 +0200

    docs: updated Document.js JSDoc documentation

--- src/Document.js
+++ src/Document.js
@@ -1,5 +1,14 @@
 import { isRawDocument, decode } from './utils.js';
 
+/**
+ * Wrapper around a fetched objects that represent a Firestore document.
+ * It is supposed to be used as a regular JS object but has a hidden
+ * property that holds the meta data of the document.
+ *
+ * That property is called `__meta__`, it should not be modified, and is non-enumerable.
+ * It is used internally to identify the document when writing the
+ * data to the database.
+ */
 export default class Document {
 	constructor(rawDoc, db) {
 		if (db === undefined) throw Error('Argument "db" is required but missing');
